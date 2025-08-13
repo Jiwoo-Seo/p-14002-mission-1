47580f5ff6617aec33125208515b793115929850
Jiwoo Seo <sjwoos123@gmail.com>
2025-08-13 12:34:29 +0900

    java to kotlin

---
+++ backend/src/main/java/com/back/domain/post/post/dto/PostWithContentDto.kt
@@ -0,0 +1,33 @@
+package com.back.domain.post.post.dto
+
+import com.back.domain.post.post.entity.Post
+import lombok.Getter
+import java.time.LocalDateTime
+
+@Getter
+class PostWithContentDto private constructor(
+    id: Int,
+    createDate: LocalDateTime,
+    modifyDate: LocalDateTime,
+    authorId: Int,
+    authorName: String,
+    title: String,
+    val content: String
+) : PostDto(
+    id,
+    createDate,
+    modifyDate,
+    authorId,
+    authorName,
+    title
+) {
+    constructor(post: Post) : this(
+        id = post.id,
+        createDate = post.createDate,
+        modifyDate = post.modifyDate,
+        authorId = post.author.id,
+        authorName = post.author.name,
+        title = post.title,
+        content = post.content
+    )
+}
