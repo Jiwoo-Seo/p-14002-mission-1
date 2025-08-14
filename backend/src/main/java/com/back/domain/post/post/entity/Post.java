8891ff04bae22c5823f9f822ae2ef601dedc638c
Jiwoo Seo <sjwoos123@gmail.com>
2025-08-14 11:31:44 +0900

    java to kotlin

--- backend/src/main/java/com/back/domain/post/post/entity/Post.java
+++ backend/src/main/java/com/back/domain/post/post/entity/Post.java
@@ -65,7 +65,7 @@ public class Post extends BaseEntity {
     public Optional<PostComment> findCommentById(int id) {
         return comments
                 .stream()
-                .filter(comment -> comment.getId() == id)
+                .filter(comment -> comment.id == id)
                 .findFirst();
     }
 
@@ -77,11 +77,11 @@ public class Post extends BaseEntity {
 
     public void checkActorCanModify(Member actor) {
         if (!author.equals(actor))
-            throw new ServiceException("403-1", "%d번 글 수정권한이 없습니다.".formatted(getId()));
+            throw new ServiceException("403-1", "%d번 글 수정권한이 없습니다.".formatted(id));
     }
 
     public void checkActorCanDelete(Member actor) {
         if (!author.equals(actor))
-            throw new ServiceException("403-2", "%d번 글 삭제권한이 없습니다.".formatted(getId()));
+            throw new ServiceException("403-2", "%d번 글 삭제권한이 없습니다.".formatted(id));
     }
 }
---
+++ backend/src/main/java/com/back/global/jpa/entity/BaseEntity.kt
@@ -0,0 +1,40 @@
+
+package com.back.global.jpa.entity
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null
+        protected set
+
+    @CreatedDate
+    var createDate: LocalDateTime? = null
+        private set
+
+    @LastModifiedDate
+    var modifyDate: LocalDateTime? = null
+        private set
+
+    protected fun setId(id: Int) {
+        this.id = id
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as BaseEntity
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = id?.hashCode() ?: 0
+}
